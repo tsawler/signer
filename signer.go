ac7ca72121eb14f33579b3cbcbd6c3ab2d63e447
Trevor Sawler <trevor.sawler@gmail.com>
2022-05-19 07:50:30 -0300

    rename var

--- signer.go
+++ signer.go
@@ -17,14 +17,14 @@ type Signature struct {
 func (s *Signature) GenerateTokenFromString(data string) string {
 	var urlToSign string
 
-	str := goalone.New([]byte(s.Secret), goalone.Timestamp)
+	pen := goalone.New([]byte(s.Secret), goalone.Timestamp)
 	if strings.Contains(data, "?") {
 		urlToSign = fmt.Sprintf("%s&hash=", data)
 	} else {
 		urlToSign = fmt.Sprintf("%s?hash=", data)
 	}
 
-	tokenBytes := str.Sign([]byte(urlToSign))
+	tokenBytes := pen.Sign([]byte(urlToSign))
 	token := string(tokenBytes)
 
 	return token
@@ -33,8 +33,8 @@ func (s *Signature) GenerateTokenFromString(data string) string {
 // VerifyToken verifies a signed token and returns true if it is valid,
 // false if it is not.
 func (s *Signature) VerifyToken(token string) bool {
-	str := goalone.New([]byte(s.Secret), goalone.Timestamp)
-	_, err := str.Unsign([]byte(token))
+	pen := goalone.New([]byte(s.Secret), goalone.Timestamp)
+	_, err := pen.Unsign([]byte(token))
 
 	if err != nil {
 		// signature is not valid. Token was tampered with, forged, or maybe it's
@@ -50,8 +50,8 @@ func (s *Signature) VerifyToken(token string) bool {
 // Expired checks to see if a token has expired. It returns true if
 // the token was created within minutesUntilExpire, and false otherwise.
 func (s *Signature) Expired(token string, minutesUntilExpire int) bool {
-	str := goalone.New([]byte(s.Secret), goalone.Timestamp)
-	ts := str.Parse([]byte(token))
+	pen := goalone.New([]byte(s.Secret), goalone.Timestamp)
+	ts := pen.Parse([]byte(token))
 
 	return time.Since(ts.Timestamp) > time.Duration(minutesUntilExpire)*time.Minute
 }
